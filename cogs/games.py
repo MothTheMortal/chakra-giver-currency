9c4cc1620c615d42a4c0a00cb609f84c356fe631
MothTheMortal <mohdmotha687@gmail.com>
2023-05-29 00:43:28 +0800

    Trying to bug find #1

--- cogs/games.py
+++ cogs/games.py
@@ -111,11 +111,11 @@ class Games(commands.Cog):
         shuffle(deck)
         dealer_hand = []
         player_hand = []
-
+        print("DECK BEFORE RUN", deck)
         for i in range(2):
             dealer_hand.append(deck.pop(0))
             player_hand.append(deck.pop(0))
-
+        print("DECK AFTER RUN", deck)
         em = self.client.create_embed(f"Blackjack", f"You bet {bet} {emoji}", config.embed_color,
                                       ctx.user.name, ctx.user.avatar.url)
 
@@ -304,6 +304,7 @@ class Games(commands.Cog):
 
             em.set_footer(text=f"Your Turn")
             await ctx.edit_original_response(embed=em)
+            print("DECK after 1st loop", deck)
 
     @app_commands.command(name="guessthenumber", description="Guess The Number Game")
     @app_commands.describe(bet="How much you want to bet (Minimum 200).",
